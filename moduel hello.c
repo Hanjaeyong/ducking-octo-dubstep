a6f9e2ae323bf76779db259dc1ee9e959a58ccef
Hanjaeyong <hjy0316@gmail.com>
2014-07-15 16:58:59 +0900

    Update moduel hello.c

--- moduel hello.c
+++ moduel hello.c
@@ -10,3 +10,11 @@ void cleanup_module(void)
 {
         printk(KERN_INFO "GOODbye world 1.\n");
 }
+
+module_init(init_hello);
+module_exit(cleanup_hello);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
+MODULE_SUPPORTED_DEVICE("testdevice");
