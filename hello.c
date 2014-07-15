eaacbf63b6470a3cec773686be446b73a9bef4d7
Hanjaeyong <hjy0316@gmail.com>
2014-07-15 16:40:18 +0900

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include<linux/module.h>
+#include<linux/kernel.h>
+
+int init_module(void)
+{
+        printk(KERN_INFO "hello world 1.\n");
+        return 0;
+}
+void cleanup_module(void)
+{
+        printk(KERN_INFO "GOODbye world 1.\n");
+}
